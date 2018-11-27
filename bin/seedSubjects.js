52770675a53c3b58ef443fa9085f2a0aba5354f5
Jaime <herrerotajaime@gmail.com>
2018-11-27 17:30:16 +0100

    seeds subject

--- bin/seedSubjects.js
+++ bin/seedSubjects.js
@@ -13,33 +13,88 @@ mongoose
 let subjects = [
   {
     title: 'Física',
-    description: `Lorem ipsum dolor sit amet consectetur adipiscing elit hendrerit,
-       scelerisque praesent purus fringilla facilisis dictumst vehicula ac consequat,
-        suspendisse odio mattis pretium quam in phasellus. `,
-    imgPath: '/images/fisica.svg'
+    description: `Ciencia que estudia las propiedades de la materia y de la energía
+    y establece las leyes que explican los fenómenos naturales.
+    Clases de física de todos los niveles, desde 2º ESO hasta preparación para EVAU`,
+    imgPath: 'https://res.cloudinary.com/dbbsyfpl1/image/upload/v1543329894/Subjects/fisica.svg'
   },
   {
     title: 'Geografía',
-    description: `Lorem ipsum dolor sit amet consectetur adipiscing elit hendrerit,
-         scelerisque praesent purus fringilla facilisis dictumst vehicula ac consequat,
-          suspendisse odio mattis pretium quam in phasellus. `,
-    imgPath: '/images/geografia.svg'
+    description: `Ciencia que estudia y describe la superficie de la Tierra en su aspecto físico,
+    actual y natural, o como lugar habitado por la humanidad.
+    Se imparten clases de geografía tanto físicas como políticas, para todos los niveles. `,
+    imgPath: 'https://res.cloudinary.com/dbbsyfpl1/image/upload/v1543329896/Subjects/geografia.svg'
   },
   {
     title: 'Literatura',
-    description: `Lorem ipsum dolor sit amet consectetur adipiscing elit hendrerit,
-           scelerisque praesent purus fringilla facilisis dictumst vehicula ac consequat,
-            suspendisse odio mattis pretium quam in phasellus. `,
-    imgPath: '/images/literatura.svg'
+    description: `Estudio de autores, generos literarios ,
+    lírica y prosa. clases enfocadas a distintos niveles,
+    incluida preparación para EVAU.`,
+    imgPath: 'https://res.cloudinary.com/dbbsyfpl1/image/upload/v1543329896/Subjects/literatura.svg'
   },
   {
     title: 'Guitarra',
-    description: `Lorem ipsum dolor sit amet consectetur adipiscing elit hendrerit,
-             scelerisque praesent purus fringilla facilisis dictumst vehicula ac consequat,
-              suspendisse odio mattis pretium quam in phasellus. `,
-    imgPath: '/images/gitarra.svg'
+    description: `Clases de guitarra por niveles. Desde principiante hasta avanzado`,
+    imgPath: 'https://res.cloudinary.com/dbbsyfpl1/image/upload/v1543329896/Subjects/guitarra.svg',
+  },
+  {
+    title: 'Historia',  
+    description:`Estudio de acotencimientos pasados.
+    Se imparten clases tanto de historia de España como de historia mundial,
+    clases por niveles, incluida preparación para EVAU.`,
+    imgPath:``
+  },
+  {
+    title: 'Filosofía',
+    description:`Estudio de autores y corrientes filosofícas. 
+    Clases de explicación y repaso, también preparación para EVAU`,
+    imgPath:``
+  },
+    
+  {
+    title:`Química`,
+    description:`Ciencia que estudia la composición y las propiedades de la materia 
+    y de las transformaciones que esta experimenta
+    sin que se alteren los elementos que la forman.
+    Clases enfocadas a formulación orgánica e inorgánica`,
+    imgPath:``
+  },
+    
+  {
+    title: `Economía`,
+    description:`Estudio de distintos modelos económicos
+     y aplicacion de forumlas finacieras. Clases para todos los niveles `,
+    imgPath:`https://res.cloudinary.com/dbbsyfpl1/image/upload/v1543329896/Subjects/economia.svg`
+  },
+  {
+    title: `Inglés`,
+    description:`Clases de vocabulario, verbos regurales e irregulares, redacción y conversacion `,
+    imgPath:``
+  },{
+    title: `Lengua`,
+    description:`Clases enfocadas a todos los cursos de la ESO y Bachillerato.
+    Analisis morfológico y sintáctico.`,
+    imgPath:`https://res.cloudinary.com/dbbsyfpl1/image/upload/v1543329896/Subjects/lengua.svg`
+  },
+  {
+    title: `Dibujo técnico`,
+    description:`Práctica y relosución de ejercicios, especialmente preparacion para EVAU`,
+    imgPath:`https://res.cloudinary.com/dbbsyfpl1/image/upload/v1543329894/Subjects/dibujoTecnico.svgs`
   },
 
+  {
+    title: `Biología`,
+    description:`Ciencia que estudia la estructura de los seres vivos y de sus procesos vitales.
+    Clases enfocadas para todos los cursos y para la EVAU`,
+    imgPath:``
+
+  },
+    {
+     title:'',
+     description:'',
+     imgPath:','
+
+    }
 ];
 
 Subject.create(subjects, (err) => {
