a03de28757deb3f73ea586f98b7688a980202921
cgandiaf <c.gandia.f@gmail.com>
2018-11-28 10:33:21 +0100

    Miércoles init

--- models/User.js
+++ models/User.js
@@ -8,14 +8,11 @@ const userSchema = new Schema({
   surname: String,
   latitude: Number,
   longitude: Number,
-  //zip: Number
-  location: { type: { type: String }, coordinates: [Number] },
   description: String,
   genre: {type: Number, enum: [0, 1, 2]},
   role: {type: String, enum: ['SOY ALUMNO', 'SOY PROFE', 'ADMON']},
   subjects: Array,
-  //subjects: {type: String, enum: ['Física', 'Geografía', 'Literatura', 'Guitarra']},
-  events: Array, //Array de Objetoeventos que aun no hay creados
+  meetings: Array, //Array de Objetoeventos que aun no hay creados
   school: String, //CON EL JSON
 }, {
   timestamps: {
--- public/javascripts/button.js
+++ public/javascripts/button.js
@@ -9,4 +9,12 @@ $(document).ready(() => {
     document.getElementById('second').classList.remove("off");
   }
 
+  document.querySelector('.back').onclick = function () {
+    // let container = document.getElementById('first');
+    // //container.classList.remove("on");
+    // container.classList.add("off");
+    document.getElementById('second').classList.add("off");
+    document.getElementById('first').classList.remove("off");
+  }
+
 })
